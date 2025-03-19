500dd6a38d8a8a728d9c7e9e1f0e4874f8ff7ab4
jheniferlorrane <jhenifer.rodrigues@sptech.school>
2025-03-19 19:40:29 -0300

    Iniciando a implementação da api calendar do google - java maven

--- ima-api/src/main/java/org/com/imaapi/ImaApiApplication.java
+++ ima-api/src/main/java/org/com/imaapi/ImaApiApplication.java
@@ -1,13 +1,30 @@
 package org.com.imaapi;
 
+import org.com.imaapi.apis.calendario.Calendario;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import com.google.api.services.calendar.model.Event;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ImaApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ImaApiApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner run() {
+        return args -> {
+            try {
+                var service = Calendario.getCalendarService();
+                List<Event> events = Calendario.getUpcomingEvents(service, 10);
+                Calendario.printUpcomingEvents(events);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+    }
 }
---
+++ validacoes/src/main/java/com/validacoes/model/BoletoBancario.java
@@ -0,0 +1,4 @@
+package com.validacoes.model;
+
+public class BoletoBancario {
+}
