f6c1547282714d61dd04607015f0f42f6e0802d2
JKeyla <joselin.cano@sptech.school>
2025-04-08 00:09:24 -0300

    subindo i v1mplementação da API

---
+++ ima-api/src/main/java/org/com/imaapi/service/EnderecoService.java
@@ -0,0 +1,39 @@
+package org.com.imaapi.service;
+
+import org.com.imaapi.model.usuario.output.EnderecoOutput;
+import org.com.imaapi.repository.EnderecoRepository;
+import org.hibernate.annotations.DialectOverride;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class EnderecoService {
+    private static final String ViaCepApi = "https://viacep.com.br/ws/%s/json/";
+    private static final Logger LOGGER = LoggerFactory.getLogger(EnderecoService.class);
+
+    private final EnderecoRepository enderecoRepository;
+
+    public EnderecoService(EnderecoRepository enderecoRepository) {
+        this.enderecoRepository = enderecoRepository;
+    }
+
+    public EnderecoOutput buscaEndereco(String cep) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = String.format(ViaCepApi, cep);
+        ResponseEntity<EnderecoOutput> response = restTemplate.getForEntity(url, EnderecoOutput.class);
+        EnderecoOutput enderecoOutput = response.getBody();
+
+        if (enderecoOutput == null || enderecoOutput.getCep() == null) {
+            throw new RuntimeException("Não consegui obter o endereço com esse CEP: " + cep);
+        }
+        LOGGER.info("CEP consultado: {}", cep);
+
+        //        EnderecoOutput EnderecoCompleto = getEndereco(enderecoOutput);
+
+        //        enderecoRepository.save(EnderecoCompleto);
+        return enderecoOutput;
+    }
+}
---
+++ ima-api/src/main/java/org/com/imaapi/service/impl/EnderecoServiceImpl.java
@@ -0,0 +1,40 @@
+//package org.com.imaapi.service.impl;
+//
+//import org.com.imaapi.model.usuario.output.EnderecoOutput;
+//import org.com.imaapi.repository.EnderecoRepository;
+//import org.com.imaapi.service.EnderecoService;
+//import org.hibernate.annotations.DialectOverride;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.client.RestTemplate;
+//
+//@Service
+//public class EnderecoServiceImpl implements EnderecoService {
+//    private static final String ViaCepApi = "https://viacep.com.br/ws/%s/json/";
+//    private static final Logger LOGGER = LoggerFactory.getLogger(EnderecoService.class);
+//
+//    private final EnderecoRepository enderecoRepository;
+//
+//    public EnderecoServiceImpl(EnderecoRepository enderecoRepository) {
+//        this.enderecoRepository = enderecoRepository;
+//    }
+//
+//    public ResponseEntity<EnderecoOutput> buscaEndereco(String cep) {
+//        RestTemplate restTemplate = new RestTemplate();
+//        String url = String.format(ViaCepApi, cep);
+//        ResponseEntity<EnderecoOutput> response = restTemplate.getForEntity(url, EnderecoOutput.class);
+//        EnderecoOutput enderecoOutput = response.getBody();
+//
+//        if (enderecoOutput == null || enderecoOutput.getCep() == null) {
+//            throw new RuntimeException("Não consegui obter o endereço com esse CEP: " + cep);
+//    }
+//        LOGGER.info("CEP consultado: {}", cep);
+//
+//        //        EnderecoOutput EnderecoCompleto = getEndereco(enderecoOutput);
+//
+//        //        enderecoRepository.save(EnderecoCompleto);
+//        return ResponseEntity.ok(enderecoOutput);
+//    }
+//}
