ff3b9b5454d6de35a08ae3bf3249567e00c6036d
SirMath200223 <matheus.osilva@sptech.school>
2025-04-13 11:47:58 -0300

    Atualização das dependências

---
+++ ima-api/src/main/java/org/com/imaapi/service/GoogleCalendar.java
@@ -0,0 +1,9 @@
+package org.com.imaapi.service;
+
+import com.google.api.client.auth.oauth2.Credential;
+
+public class GoogleCalendar {
+    private static final CalendarList calendarList = new CalendarList();
+
+
+}
