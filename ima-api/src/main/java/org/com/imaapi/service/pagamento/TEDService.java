62660b27d48c0fac36c37645b5737e01bc613683
giovannagomeslm <giovanna.macedo@sptech.school>
2025-05-19 20:07:40 -0300

    Inserindo o service dos métodos de pagamento

--- ima-api/src/main/java/org/com/imaapi/service/pagamento/TEDService.java
+++ ima-api/src/main/java/org/com/imaapi/service/pagamento/TEDService.java
@@ -1,4 +1,43 @@
 package org.com.imaapi.service.pagamento;
 
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.com.imaapi.configPagamento.ConfigCoraPagamento;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+@Service
 public class TEDService {
-}
+
+        @Autowired
+        private ConfigCoraPagamento.CoraApiConfig config;
+
+        public <TedTransferRequest> String realizarTed(TedTransferRequest request, String token) throws Exception {
+            URL url = new URL(config.getBaseUrl() + "/v2/transfers");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Authorization", "Bearer " + token);
+            conn.setDoOutput(true);
+
+            String json = new ObjectMapper().writeValueAsString(request);
+            OutputStream os = conn.getOutputStream();
+            os.write(json.getBytes());
+            os.flush();
+
+            if (conn.getResponseCode() != 200 && conn.getResponseCode() != 201) {
+                throw new RuntimeException("Erro ao realizar TED. Código: " + conn.getResponseCode());
+            }
+
+            return "TED realizada com sucesso!";
+        }
+
+
+
+
+        }
