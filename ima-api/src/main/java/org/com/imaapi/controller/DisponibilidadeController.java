b3141b88801a0c6186f3d5a321e2d5af6836d311
jheniferlorrane <jhenifer.rodrigues@sptech.school>
2025-05-28 22:36:22 -0300

    Update

---
+++ ima-api/src/main/java/org/com/imaapi/controller/DisponibilidadeController.java
@@ -0,0 +1,9 @@
+package org.com.imaapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/disponibilidade")
+public class DisponibilidadeController {
+}
