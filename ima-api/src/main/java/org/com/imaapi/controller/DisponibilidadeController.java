3b153031a16a5ef9850a0d74f6c8f7b4f30a720c
ryangsantos2 <160976028+ryangsantos2@users.noreply.github.com>
2025-05-29 23:38:07 -0300

    Realizada criação de rota para busca de horarios disponiveis (#44)

--- ima-api/src/main/java/org/com/imaapi/controller/DisponibilidadeController.java
+++ ima-api/src/main/java/org/com/imaapi/controller/DisponibilidadeController.java
@@ -1,9 +1,24 @@
 package org.com.imaapi.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.com.imaapi.service.impl.DisponibilidadeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/disponibilidade")
 public class DisponibilidadeController {
+    @Autowired
+    private DisponibilidadeServiceImpl disponibilidadeService;
+
+    @GetMapping("/{voluntarioId}/horarios")
+    public List<LocalTime> getHorariosDisponiveis(
+            @PathVariable Integer voluntarioId,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate data) {
+        return disponibilidadeService.buscarHorariosDisponiveis(voluntarioId, data);
+    }
 }
